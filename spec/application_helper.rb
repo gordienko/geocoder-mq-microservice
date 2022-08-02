7129cbb41bd69f9cd47a94cf1380beeccc271bda
Alexey Gordienko <alx@anadyr.org>
2022-08-02 14:55:20 +0300

    first commit

--- spec/application_helper.rb
+++ spec/application_helper.rb
@@ -10,7 +10,3 @@ require_relative '../config/environmant'
 
 abort('You run tests in production mode. Please don\'t do this!') if Application.environment == :production
 Dir[Application.root.concat('/spec/support/**/*.rb')].sort.each { |f| require f }
-
-RSpec.configure do |config|
-  config.include RouteHelpers, type: :routes
-end
--- spec/support/request_helpers.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module RequestHelpers
-  def response_body
-    JSON(response.body)
-  end
-end
